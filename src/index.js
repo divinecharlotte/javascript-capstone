f0d2fb1a349c117cb9369cd09d2e2d43e3bbebf1
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-21 17:04:32 +0300

    Setup wabpack and set Header

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import _ from 'lodash';
+import './styles/style.css';
+const apiliks = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NeJSPpMrADKWN3Hg0NY2/likes'
+const apicom = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NeJSPpMrADKWN3Hg0NY2/comments'
+const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=vegetarian'
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    path: path.resolve(__dirname, 'src/index.js'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
