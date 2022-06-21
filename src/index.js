b1a68e64ab67b1d4ec4d8b0de51d17ac5274eccc
charlotte-divine <divinemaina@gmail.com>
2022-06-21 22:13:36 +0100

    Display list of items on the Homepage - Student A #15

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import _ from 'lodash';
-import './styles/style.css';
-const apiliks = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NeJSPpMrADKWN3Hg0NY2/likes'
-const apicom = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/NeJSPpMrADKWN3Hg0NY2/comments'
-const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=vegetarian'
+import "./styles/style.css";
+import displayCards from "./modules/itemslist.js";
+const recipeCount = document.querySelector('.item-count');
+displayCards();
---
+++ src/modules/itemslist.js
@@ -0,0 +1,34 @@
+
+const recipeCount = document.querySelector('.item-count');
+const recipeCards = document.querySelector('.recipe-cards');
+
+const displayCards = async () => {
+ const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian');
+ const data = await response.json();
+     
+      for(let i = 0;i<20;i++) {
+        data.meals[i].idMeal = i;
+      }
+      console.log(data);
+      const file = data.meals;
+      const newArray = file.filter((Objects) =>  Objects.idMeal <= 11);
+      console.log(newArray);
+      let html = '';
+      newArray.forEach((meal) => {
+        html += `<div class="card" id="${meal.idMeal}">
+               <div class="card-img">
+                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+               </div>
+               <div class="card-info">
+                   <h2>${meal.strMeal}</h2>
+                   <ul>
+                       <li><button id=${meal.idMeal}  class="comment-btn" type="button">Comments</button></li>
+                       <li><button class="like-btn"><i class="fa-regular fa-heart"></i><span class="like-count">0</span></button></li>
+                   </ul>
+               </div>
+             </div>`;
+        recipeCards.innerHTML = html;
+      });
+  };
+
+export default displayCards;
