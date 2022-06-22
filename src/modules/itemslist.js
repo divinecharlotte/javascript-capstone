faf516424ca0c2781a9193e6b508d2904566317e
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-22 10:56:39 +0300

    Fix Linter

--- src/modules/itemslist.js
+++ src/modules/itemslist.js
@@ -1,30 +1,28 @@
-const recipeCount = document.querySelector(".item-count");
-const recipeCards = document.querySelector(".recipe-cards");
+const recipeCount = document.querySelector('.item-count');
+const recipeCards = document.querySelector('.recipe-cards');
 
 const displayCards = async () => {
   const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian"
+    'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian'
   );
   const data = await response.json();
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 20; i += 1) {
     data.meals[i].idMeal = i;
   }
-  console.log(data);
   const file = data.meals;
   const newArray = file.filter((Objects) => Objects.idMeal <= 11);
-  console.log(newArray);
-  let html = "";
+  let html = '';
   newArray.forEach((meal) => {
-    html += `<div class="card" id="${meal.idMeal}">
-               <div class="card-img">
-                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+    html += `<div class='card' id='${meal.idMeal}'>
+               <div class='card-img'>
+                   <img src='${meal.strMealThumb}' alt='${meal.strMeal}'>
                </div>
-               <div class="card-info">
+               <div class='card-info'>
                    <h2>${meal.strMeal}</h2>
                    <ul>
-                       <li><button id=${meal.idMeal}  class="comment-btn" type="button">Comments</button></li>
-                       <li><button class="like-btn"><i class="fa-regular fa-heart"></i><span class="like-count">0</span></button></li>
+                       <li><button id=${meal.idMeal}  class='comment-btn' type='button'>Comments</button></li>
+                       <li><button class='like-btn'><i class='fa-regular fa-heart'></i><span class='like-count'>0</span></button></li>
                    </ul>
                </div>
              </div>`;
