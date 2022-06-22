5d89b530485eedddb5440478b45f6b85a71daa51
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-06-22 21:58:58 +0300

    Add popup for comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './styles/style.css';
 import displayCards from './modules/itemslist.js';
+import { doet } from './modules/comment.js';
 
 displayCards();
+
+const btn = document.querySelector('main');
+btn.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) {
+    const b = e.target.id;
+    doet(b);
+  }
+});
